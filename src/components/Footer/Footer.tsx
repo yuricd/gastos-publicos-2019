ed919cd921ec6dd542506193b66e2fc4e49973c2
Yuri Catarino <ycatarino@localhost.localdomain>
2019-12-29 22:30:22 -0300

    Fixes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
   return (
     <footer className={`${styles.generalFooter} ${styles.slideLeft2s}`}>
       <p>Dados públicos obtidos em <a href="https://www2.camara.leg.br/transparencia/cota-para-exercicio-da-atividade-parlamentar/dados-abertos-cota-parlamentar" target="_blank" rel="noopener noreferrer">https://www2.camara.leg.br</a><br /> em 28/12/2019.</p>
-      <p>Projeto de <a href="/" target="_blank" rel="noopener noreferrer">código aberto</a>.</p>
+      <p>Projeto de <a href="https://github.com/yuricd/gastos-publicos-2019" target="_blank" rel="noopener noreferrer">código aberto</a>.</p>
     </footer>
   );
 };
